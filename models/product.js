4dea3789a8d22bf87250c17572e30f5e2530c15e
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2019-11-09 23:25:41 +0200

    dynamic products

--- models/product.js
+++ models/product.js
@@ -14,8 +14,11 @@ const getProducsFromFile = cb =>{
 
 const products = [];
 module.exports = class Product {
-    constructor(title,){
+    constructor(title,imageUrl,description,price){
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
         save(){
