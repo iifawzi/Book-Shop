4276f4186d258cfa652f05376218395fa534b3df
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2019-11-09 23:46:16 +0200

    adding orders page

--- routes/shop.js
+++ routes/shop.js
@@ -12,6 +12,10 @@ router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
 
+router.get('/orders', shopController.getOrders);
+
 router.get('/checkout', shopController.getCheckout);
+
+
 
 module.exports = router;
